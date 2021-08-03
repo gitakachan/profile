2006e04f4b82e51e4021e50002a77a9a7ddc266a
Cheni Chen <celiachenic@gmail.com>
2021-08-03 23:01:25 +0800

    基本環境設置

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+//這裡寫的配置會和其他配置結合
+const path = require("path");
+
+module.exports = {
+  //配置別名
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // "@": "src", //已經默認配置
+        assets: "@/assets",
+        components: "@/components",
+      },
+    },
+  },
+  //vue-cli-plugin-style-resources-loader
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: ['/Users/chenic/Desktop/projects/profile/profile/src/assets/scss/_variables.scss'],
+    },
+  },
+
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-mini-projects01/" : "/",
+};
