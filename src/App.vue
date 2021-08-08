411d3057bf6fbfca74eadd7978f54bbbc735e65d
Cheni Chen <celiachenic@gmail.com>
2021-08-08 14:10:02 +0800

    footer icon attribute and copyright

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <main-nav-bar></main-nav-bar>
     <router-view></router-view>
+    <main-footer></main-footer>
   </div>
 </template>
 
 <script>
 //components
 import MainNavBar from "./components/common/navBar/MainNavBar.vue";
+import MainFooter from "./components/common/footer/MainFooter.vue";
+
 export default {
   name: "App",
   components: {
     MainNavBar,
+    MainFooter,
   },
 };
 </script>
---
+++ src/components/common/footer/MainFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="fixed-bottom text-center">
+    <div class="copyright">
+      <p>Created by Cheni Chen. <span>&copy;</span> 2021</p>
+    </div>
+    <div class="attribute">
+      Icon made by
+      <a href="https://www.freepik.com" title="Freepik">Freepik</a> from
+      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss"></style>
